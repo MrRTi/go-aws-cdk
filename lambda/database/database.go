7558881767bfc6d140c1d8d7a559f43aaeaf83fc
Artem Musalitin <musalitin.a.a@gmail.com>
2024-05-27 18:05:12 +0200

    Add db user functions

--- lambda/database/database.go
+++ lambda/database/database.go
@@ -8,6 +8,9 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
+const (
+	USERS_TABLE_NAME = "users"
+)
 
 type DynamoDBClient struct {
 	databaseStore *dynamodb.DynamoDB
@@ -22,6 +25,45 @@ func NewDynamoDBClient() DynamoDBClient {
 	}
 }
 
+func (db DynamoDBClient) UserExists(username string) (bool, error) {
+	result, err := db.databaseStore.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(USERS_TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(username),
+			},
+		},
+	})
 
+	if err != nil {
+		return true, err
+	}
 
+	if result.Item == nil {
+		return false, nil
+	}
 
+	return true, nil
+}
+
+func (db DynamoDBClient) InsertUser(user types.RegisterUser) error {
+	item := &dynamodb.PutItemInput{
+		TableName: aws.String(USERS_TABLE_NAME),
+		Item: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(user.Username),
+			},
+			"password": {
+				S: aws.String(user.Password),
+			},
+		},
+	}
+
+	_, err := db.databaseStore.PutItem(item)
+	
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
