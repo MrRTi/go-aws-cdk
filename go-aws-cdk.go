846854e73444b66d7ad559a059280bccd60e95e5
Artem Musalitin <musalitin.a.a@gmail.com>
2024-05-27 18:29:38 +0200

    Add dynamo db

--- go-aws-cdk.go
+++ go-aws-cdk.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -18,11 +19,22 @@ func NewGoAwsCdkStack(scope constructs.Construct, id string, props *GoAwsCdkStac
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	awslambda.NewFunction(stack, jsii.String("lambdaFunc"), &awslambda.FunctionProps{
+	table := awsdynamodb.NewTable(stack, jsii.String("usersTable"), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("username"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		TableName:     jsii.String("users"),
+		RemovalPolicy: awscdk.RemovalPolicy_DESTROY,
+	})
+
+	lambda := awslambda.NewFunction(stack, jsii.String("lambdaFunc"), &awslambda.FunctionProps{
 		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
-		Code: awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
+		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
 		Handler: jsii.String("main"),
 	})
+
+	table.GrantReadWriteData(lambda)
 
 	return stack
 }
