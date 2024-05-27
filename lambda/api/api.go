1455eee420c3b0823d5696cf26a37432b1b1ad36
Artem Musalitin <musalitin.a.a@gmail.com>
2024-05-27 18:16:04 +0200

    Add api logic

--- lambda/api/api.go
+++ lambda/api/api.go
@@ -1,6 +1,10 @@
 package api
 
-import "lambda-func/database"
+import (
+	"fmt"
+	"lambda-func/database"
+	"lambda-func/types"
+)
 
 type ApiHandler struct {
 	dbStore database.DynamoDBClient
@@ -10,4 +14,28 @@ func NewApiHandler(dbStore database.DynamoDBClient) ApiHandler {
 	return ApiHandler{
 		dbStore: dbStore,
 	}
+}
+
+func (api ApiHandler) RegisterUserHandler(event types.RegisterUser) error {
+	if event.Username == "" || event.Password == "" {
+		return fmt.Errorf("Bad request: username and passord are required")
+	}
+
+	userExists, err := api.dbStore.UserExists(event.Username)
+
+	if err != nil {
+		return fmt.Errorf("Error: %w", err)
+	}
+
+	if userExists {
+		return fmt.Errorf("User with that username already exists")
+	}
+
+	err = api.dbStore.InsertUser(event)
+
+	if err != nil {
+		return fmt.Errorf("Error when creating user: %w", err)
+	}
+
+	return nil
 }
