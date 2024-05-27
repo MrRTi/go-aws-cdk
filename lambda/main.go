10d0df3bb524b47508a641c1b0d46aa3c04b0008
Artem Musalitin <musalitin.a.a@gmail.com>
2024-05-27 18:31:01 +0200

    Use app in lambda

--- lambda/main.go
+++ lambda/main.go
@@ -1,25 +1,12 @@
 package main
 
 import (
-	"fmt"
-	// "lambda-func/app"
+	"lambda-func/app"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Username string `json:"username"`
-}
-
-func HandleRequest(event MyEvent) (string, error) {
-	if event.Username == "" {
-		return "", fmt.Errorf("Username cannot be empty")
-	}
-
-	return fmt.Sprintf("Successfully called by - %s", event.Username), nil
-}
-
 func main() {
-	// _ := app.NewApp()
-	lambda.Start(HandleRequest)
+	app := app.NewApp()
+	lambda.Start(app.ApiHandler.RegisterUserHandler)
 }
