edafd750a717b476b6f68f78a27a3db80a7d9785
Artem Musalitin <musalitin.a.a@gmail.com>
2024-05-28 18:34:16 +0200

    Add protected endpoint

--- lambda/main.go
+++ lambda/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"lambda-func/app"
+	"lambda-func/middleware"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+func ProtectedHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       "This is a secret path",
+		StatusCode: http.StatusOK,
+	}, nil
+}
 
 func main() {
 	app := app.NewApp()
@@ -16,6 +24,8 @@ func main() {
 			return app.ApiHandler.RegisterUserHandler(request)
 		case "/login":
 			return app.ApiHandler.LoginUserHandler(request)
+		case "/protected":
+			return middleware.ValidateJWTMiddleware(ProtectedHandler)(request)
 		default:
 			return events.APIGatewayProxyResponse{
 				Body:       "Not found",
